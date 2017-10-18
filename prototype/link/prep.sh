90b7585b76e435c4f030ba583804c6f326a39aba
Mariam Aly <mariam.s.aly@gmail.com>
2017-10-17 22:05:18 -0400

    minor update to keep track of steps running

--- prototype/link/prep.sh
+++ prototype/link/prep.sh
@@ -8,7 +8,24 @@ set -e
 
 source globals.sh
 
+
+echo "==Prep (1) : convert and wrap raw data =="
+echo ' '
 bash scripts/convert-and-wrap-raw-data.sh $DICOM_ARCHIVE $NIFTI_DIR $SUBJ $RUNORDER_FILE
+
+echo "==Prep (2) : qa-wrapped-data=="
+echo ' '
 bash scripts/qa-wrapped-data.sh $NIFTI_DIR $QA_DIR
+
+echo "==Prep (3) : reorient to las=="
+echo ' '
 bash scripts/reorient-to-las.sh $NIFTI_DIR
+
+echo "==Prep (4) : B0 unwarp prep=="
+echo ' '
+# bash scripts/B0unwarpPrep.sh $NIFTI_DIR $SUBJ
+
+echo "==Prep (5) : render-fsf-template=="
+echo ' '
 bash scripts/render-fsf-templates.sh
+
